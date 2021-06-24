3e27d29313c31cab8a0c75ef0c8e4253de769769
EDER DANIEL DOMINGUES <60990935+ederdaniel22@users.noreply.github.com>
2021-06-24 18:32:53 -0400

    Criação das salas
    Criação das salas com React, Firebase e estilizando com SCSS

--- letmeask-2/src/pages/Home.tsx
+++ letmeask-2/src/pages/Home.tsx
@@ -1,32 +1,74 @@
+import { useHistory } from 'react-router'
+import { FormEvent } from 'react'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
+import { database } from '../services/firebase'
 
-export function Home(){
-  return(
-    <div>
+import { Button } from '../components/Button'
+import { useAuth } from '../hooks/useAuth'
+
+import '../styles/auth.scss'
+import { useState } from 'react'
+
+export function Home() {
+  const history = useHistory()
+  const { user, signInWithGoogle } = useAuth()
+  const [roomCode, setRoomCode] = useState('')
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
+    }
+
+    history.push('/rooms/new')
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault()
+
+    if (roomCode.trim() === '') {
+      return
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists')
+      return
+    }
+
+    history.push(`/rooms/${roomCode}`)
+  }
+
+  return (
+    <div id="page-auth">
       <aside>
-        <img src={illustrationImg}alt="Ilustração simbolizando perguntas e respostas" />
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
         <strong>Crie salas de Q&amp;A ao vivo</strong>
         <p>Tire suas dúvidas em tempo real</p>
       </aside>
 
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button>
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o google
           </button>
-          <div>ou entre em uma sala</div>
-          <form>
-            <input 
-            type="text" 
-            placeholder="Digite o código da sala"
+          <div className="separator">ou entre em uma sala</div>
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode}
             />
-            <button type="submit">
-              Entrar na sala
-            </button>
+            <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
       </main>
